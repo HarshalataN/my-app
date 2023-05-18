bdfcf139be0934eb6b373d918d6804e804cddc11
HarshalataN <harshalatanikhade53@gmail.com>
2023-05-18 15:16:10 +0530

    2nd commit

--- src/component/Add/Add.js
+++ src/component/Add/Add.js
@@ -1,7 +1,7 @@
 const Add = (props) => {                               // Add.js is not component and Add function is component
     return (
         <div>                                          
-            <p>This is Add component</p>
+            <p>This is Add component 1</p>
             <p>Add ={parseInt(props.a)+parseInt(props.b)}</p>                   
         </div>
     )
